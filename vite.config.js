5f4cbe008c8a2213012d97b3e9adad4cff76b998
aiko <aiko@Mac.lan>
2026-06-28 18:49:34 -0700

    Serve Vault landing page from root domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/vault/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
